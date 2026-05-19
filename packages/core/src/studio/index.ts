341f017d0b30ad15d9bfb681fb71cafc9539b606
Tim Dacalos <2004tim2004@gmail.com>
2026-05-18 22:38:35 -0700

    Track 1+2 done

---
+++ packages/core/src/studio/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Studio module barrel export.
+ *
+ * @module @iem/core/studio
+ * @track studio_manifest_types_20260504 (Track 2)
+ */
+
+export * from "./contracts.js";
+export * from "./schemas.js";
